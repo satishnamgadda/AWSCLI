f96b12ba49a720b47b074523d597b4eb0ffb93d7
ns <namgaddasatish@gmail.com>
2022-10-27 13:45:18 +0530

    added files

---
+++ ec2.sh
@@ -0,0 +1,115 @@
+AWS_REGION="ap-south-1"
+VPC_NAME="My VPC11"
+VPC_CIDR="10.0.0.0/16"
+SUBNET_PUBLIC_CIDR="10.0.1.0/24"
+SUBNET_PUBLIC_AZ="ap-south-1a"
+SUBNET_PUBLIC_NAME="public subnet"
+SUBNET_PRIVATE_CIDR="10.0.2.0/24"
+SUBNET_PRIVATE_AZ="ap-south-1b"
+SUBNET_PRIVATE_NAME="private subnet"
+
+------------------------------------------------------
+#
+# Create VPC
+echo "Creating VPC in preferred region..."
+VPC_ID=$(aws ec2 create-vpc \
+  --cidr-block $VPC_CIDR \
+  --query 'Vpc.{VpcId:VpcId}' \
+  --output text \
+  --region $AWS_REGION)
+echo "  VPC ID '$VPC_ID' CREATED in '$AWS_REGION' region."
+
+# Add Name tag to VPC
+aws ec2 create-tags \
+  --resources $VPC_ID \
+  --tags "Key=Name,Value=$VPC_NAME" \
+  --region $AWS_REGION
+echo "  VPC ID '$VPC_ID' NAMED as '$VPC_NAME'."
+
+# Create Public Subnet
+echo "Creating Public Subnet..."
+SUBNET_PUBLIC_ID=$(aws ec2 create-subnet \
+  --vpc-id $VPC_ID \
+  --cidr-block $SUBNET_PUBLIC_CIDR \
+  --availability-zone $SUBNET_PUBLIC_AZ \
+  --query 'Subnet.{SubnetId:SubnetId}' \
+  --output text \
+  --region $AWS_REGION)
+echo "  Subnet ID '$SUBNET_PUBLIC_ID' CREATED in '$SUBNET_PUBLIC_AZ'" \
+  "Availability Zone."
+
+# Add Name tag to Public Subnet
+aws ec2 create-tags \
+  --resources $SUBNET_PUBLIC_ID \
+  --tags "Key=Name,Value=$SUBNET_PUBLIC_NAME" \
+  --region $AWS_REGION
+echo "  Subnet ID '$SUBNET_PUBLIC_ID' NAMED as" \
+  "'$SUBNET_PUBLIC_NAME'."
+
+# Create Private Subnet
+echo "Creating Private Subnet..."
+SUBNET_PRIVATE_ID=$(aws ec2 create-subnet \
+  --vpc-id $VPC_ID \
+  --cidr-block $SUBNET_PRIVATE_CIDR \
+  --availability-zone $SUBNET_PRIVATE_AZ \
+  --query 'Subnet.{SubnetId:SubnetId}' \
+  --output text \
+  --region $AWS_REGION)
+echo "  Subnet ID '$SUBNET_PRIVATE_ID' CREATED in '$SUBNET_PRIVATE_AZ'" \
+  "Availability Zone."
+
+# Add Name tag to Private Subnet
+aws ec2 create-tags \
+  --resources $SUBNET_PRIVATE_ID \
+  --tags "Key=Name,Value=$SUBNET_PRIVATE_NAME" \
+  --region $AWS_REGION
+echo "  Subnet ID '$SUBNET_PRIVATE_ID' NAMED as '$SUBNET_PRIVATE_NAME'."
+
+# Create Internet gateway
+echo "Creating Internet Gateway..."
+IGW_ID=$(aws ec2 create-internet-gateway \
+  --query 'InternetGateway.{InternetGatewayId:InternetGatewayId}' \
+  --output text \
+  --region $AWS_REGION)
+echo "  Internet Gateway ID '$IGW_ID' CREATED."
+
+# Attach Internet gateway to your VPC
+aws ec2 attach-internet-gateway \
+  --vpc-id $VPC_ID \
+  --internet-gateway-id $IGW_ID \
+  --region $AWS_REGION
+echo "  Internet Gateway ID '$IGW_ID' ATTACHED to VPC ID '$VPC_ID'."
+
+# Create Route Table
+echo "Creating Route Table..."
+ROUTE_TABLE_ID=$(aws ec2 create-route-table \
+  --vpc-id $VPC_ID \
+  --query 'RouteTable.{RouteTableId:RouteTableId}' \
+  --output text \
+  --region $AWS_REGION)
+echo "  Route Table ID '$ROUTE_TABLE_ID' CREATED."
+
+# Create route to Internet Gateway
+RESULT=$(aws ec2 create-route \
+  --route-table-id $ROUTE_TABLE_ID \
+  --destination-cidr-block 0.0.0.0/0 \
+  --gateway-id $IGW_ID \
+  --region $AWS_REGION)
+echo "  Route to '0.0.0.0/0' via Internet Gateway ID '$IGW_ID' ADDED to" \
+  "Route Table ID '$ROUTE_TABLE_ID'."
+
+# Associate Public Subnet with Route Table
+RESULT=$(aws ec2 associate-route-table  \
+  --subnet-id $SUBNET_PUBLIC_ID \
+  --route-table-id $ROUTE_TABLE_ID \
+  --region $AWS_REGION)
+echo "  Public Subnet ID '$SUBNET_PUBLIC_ID' ASSOCIATED with Route Table ID" \
+  "'$ROUTE_TABLE_ID'."
+
+# Enable Auto-assign Public IP on Public Subnet
+aws ec2 modify-subnet-attribute \
+  --subnet-id $SUBNET_PUBLIC_ID \
+  --map-public-ip-on-launch \
+  --region $AWS_REGION
+echo "  'Auto-assign Public IP' ENABLED on Public Subnet ID" \
+  "'$SUBNET_PUBLIC_ID'."
